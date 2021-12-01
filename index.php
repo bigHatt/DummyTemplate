48f88a59e20af3f7e35c55f14ce91d02c23abea2
BigHat <33033974+bigHatt@users.noreply.github.com>
2021-12-01 19:43:17 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once __DIR__.'/vendor/autoload.php';
 
 use App\Template\ReverseTemplate;
 
-$t2 = 'Hello, my name is {{name}}. Oh my is {name2}}, and he is {name3}';
+$t2 = 'Hello, my name is {{name}}. Oh my is {{name2}}, and he is {name3}';
 $string = "Hello, my name is Dio. Oh my is Jotaro, and he is Polnareff";
 $reverse = new ReverseTemplate($t2, $string);
 $props = $reverse->getProperties();
